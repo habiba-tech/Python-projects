de49a55f060e848b88b6706c89cb7b18eb13dbe9
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2026-04-20 20:40:26 +0530

    Update and rename temp.py to List.py

---
+++ DSA/List.py
@@ -0,0 +1,22 @@
+names =["habiba","muskan","Anam","Ruhi"]
+print(names)
+print(type(names))
+print(len(names)) # checking len 
+print(names[0]) #checking indexing
+print(names)
+names[0]="Rubi" #update 
+print(names)
+names.append("saima")
+print(names)
+names.insert(3,"sufi")
+print(names)
+names.extend("hiba")
+print(names)
+names.remove("saima")
+print(names)
+names.pop()
+print(names)
+a=[2,6,100,6,7,4,8]
+print(min(a))
+print(max(a))
+
