f89e3f0d92221298e7a903497f23f076d6aa3fb0
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2026-04-21 15:08:31 +0530

    Create tuple.py with basic tuple operations
    Add a new file to demonstrate tuple operations in Python.

---
+++ DSA/tuple.py
@@ -0,0 +1,15 @@
+#Tuple 
+a=(4,5,2,6,7,8,1,200)
+print(a)
+
+#checking the length of tuple
+print (len(a))
+
+#checking the index
+print (a[0])
+
+#checking negative indexing
+print(a[-1])
+
+#silcing
+print(a[4:8])
