cb812c181fdc1aa6af71034d082f5c379f71ecd1
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2026-04-23 20:05:59 +0530

    Create String.py

---
+++ DSA/String.py
@@ -0,0 +1,19 @@
+#string
+
+name= "habiba"
+grade = "A"
+
+# multiline connents
+introduction = """My mane is habiba
+I Live in Mumbai 
+I love coding """
+
+print(name,type(name))
+
+#operations
+print(len(introduction))
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.strip())
+print(introduction.replace("habiba","Muskan"))
